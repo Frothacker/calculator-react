dc1feaee403a8ad592cfeb8dc21589584e6416fe
Finn <finn.rothacker@gmail.com>
2018-06-25 17:00:14 +0800

    created working number table and input bar. all under one class thoguh, will need to separate components out, which will make me learn how to pass props / functions.

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './index.css';
+import { Input, Container, Grid } from 'semantic-ui-react';
 
-const Calculator = () => {
-  return <h1></h1>;
+
+function Cell(props) {
+  return(
+    <Grid.Column className='cell' onClick={ () => {console.log(props.value)} }>
+      {props.value}
+    </Grid.Column>
+  );
 }
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      content: ""
+    }
+  }
+
+  renderCell(i) {
+    return (
+      <Grid.Column 
+      className='cell' 
+      onClick={ () => this.setState({content: this.state.content + i }) }
+      >
+      {i}
+      </Grid.Column>
+    );
+  }
+
+  renderInputBar() {
+    return(
+      <Input 
+      placeholder="numbers"
+      value={this.state.content}
+      fluid 
+      onChange={(event) => this.setState({ content: event.target.value })}
+      />
+    );
+  }
+
+  render() {
+    return(
+      <Container text className="container">
+        {this.renderInputBar()}
+        <Grid columns={3} celled>
+          <Grid.Row>
+            {this.renderCell(1)}
+            {this.renderCell(2)}
+            {this.renderCell(3)}
+          </Grid.Row>
+          <Grid.Row>
+            {this.renderCell(4)}
+            {this.renderCell(5)}
+            {this.renderCell(6)}
+          </Grid.Row>
+          <Grid.Row>
+            {this.renderCell(7)}
+            {this.renderCell(8)}
+            {this.renderCell(9)}
+          </Grid.Row>
+        </Grid>
+      </Container>
+    );
+  }
+}
+
 
 export default Calculator;
--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,6 +1,5 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Calculator from './Calculator';
 
 ReactDOM.render(<Calculator />, document.getElementById('root'));
