0a2c63b2670dafa2b67c9e99db0cdd3567f9766f
Finn <finn.rothacker@gmail.com>
2018-06-25 19:30:55 +0800

    more work on calculator, have not yet refactored out components, but i added operators, in the process of making them work

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -15,9 +15,15 @@ class Calculator extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      content: ""
+      answer: null,
+      content: "0",
+      number: null,
+      placeholder: "", 
+      operator: null
     }
   }
+
+  // this.addEventListener('keypress', (event)=> { this.setState({ content: this.state.content + event.target.value }) });
 
   renderCell(i) {
     return (
@@ -30,10 +36,59 @@ class Calculator extends Component {
     );
   }
 
+  renderOperator(i) {
+    let x = this.state
+    return (
+      <Grid.Column 
+      className='operator' 
+      onClick={ () => {
+          this.setState({
+            number: x.content,
+            placeholder: x.content, 
+            content: '',
+            operator: {i} }) 
+        } // function end
+      }> 
+      {i}
+      </Grid.Column>
+    );
+  }
+
+  renderEquals(i) {
+    let x = this.state
+    let result = 0
+    return (
+      <Grid.Column 
+      className='operator' 
+      onClick={ () => {
+        if (x.operator.i === "-") {
+          result = x.number - x.content
+        } else if (x.operator.i === "+") {
+          result = x.number + x.content
+        } else if (x.operator.i === "%") {
+          result = x.number / x.content
+        } else {
+          result = "there was no match in the if block"
+        }
+
+        this.setState({
+          answer: result,
+          content: result,
+          number: result,
+          placeholder: ''
+        })
+           
+        } // function end
+      }> 
+      =
+      </Grid.Column>
+    );
+  }
+
   renderInputBar() {
     return(
       <Input 
-      placeholder="numbers"
+      placeholder={this.state.placeholder}
       value={this.state.content}
       fluid 
       onChange={(event) => this.setState({ content: event.target.value })}
@@ -41,25 +96,32 @@ class Calculator extends Component {
     );
   }
 
+
   render() {
     return(
       <Container text className="container">
         {this.renderInputBar()}
-        <Grid columns={3} celled>
+        <Grid columns={4} celled>
           <Grid.Row>
             {this.renderCell(1)}
             {this.renderCell(2)}
             {this.renderCell(3)}
+            {this.renderOperator("-")}
           </Grid.Row>
           <Grid.Row>
             {this.renderCell(4)}
             {this.renderCell(5)}
             {this.renderCell(6)}
+            {this.renderOperator("%")}
           </Grid.Row>
           <Grid.Row>
             {this.renderCell(7)}
             {this.renderCell(8)}
             {this.renderCell(9)}
+            {this.renderOperator("+")}
+          </Grid.Row>
+          <Grid.Row>
+            {this.renderEquals()}
           </Grid.Row>
         </Grid>
       </Container>
