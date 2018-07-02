24bf5d2af6c86f0723ee5ba3aa0fe47fa73948f6
Finn <finn.rothacker@gmail.com>
2018-07-02 14:15:40 +0800

    frz

--- calculator/src/buttons/Operator.js
+++ calculator/src/buttons/Operator.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Grid } from "semantic-ui-react";
 
 function Operator(props) {
-    let operatorStyle = { backgroundColor: "#FFA500" }
-    if (props.i === props.style) {
-      operatorStyle = { backgroundColor: "#D88C00"}
-    }
-    return (
-      <Grid.Column 
-      style={ operatorStyle } 
-      onMouseDown={ props.onClick }> 
-      {props.i}
-      </Grid.Column>
-    );
+  let operatorStyle = { backgroundColor: "#FFA500" }
+  if (props.i === props.style) {
+    operatorStyle = { backgroundColor: "#D88C00"}
   }
+  return (
+    <Grid.Column 
+    style={ operatorStyle } 
+    onMouseDown={ props.onClick }> 
+    {props.i}
+    </Grid.Column>
+  );
+}
 
-  export default Operator;
+export default Operator;
